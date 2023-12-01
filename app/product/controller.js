5519bce52fdccd949d624077a66d49356debedba
DaniHidayat <danihidayat015@gmail.com>
2023-12-01 13:51:37 +0700

    fixing seach

--- app/product/controller.js
+++ app/product/controller.js
@@ -23,33 +23,35 @@ module.exports = {
           res.status(500).json({message: err.message || 'internal server error'});
       }
   },
-    listProdukSearch: async (req, res) => {
-      try {
-        // Extract search parameters from the request query
-        const { keyword } = req.query;
-    
-        // Define a basic query to find all products
-        let query = {};
-    
-        // If a keyword is provided, add a search condition
-        if (keyword) {
-          query = {
-            $or: [
-              { name: { $regex: new RegExp(keyword, 'i') } }, // Case-insensitive name search
-              { categori: { $regex: new RegExp(keyword, 'i') } }, // Case-insensitive category search
-              // Add more fields to search if needed
-            ],
-          };
-        }
-    
-        // Use the query to find products
-        const products = await Product.find(query);
-    
-        res.status(200).json({ data: products });
-      } catch (err) {
-        res.status(500).json({ message: err.message || 'Internal server error' });
+  listProdukSearch: async (req, res) => {
+    try {
+      // Extract search parameters from the request query
+      const { keyword } = req.query;
+  
+      // Define a basic query to find all products
+      let query = {};
+  
+      // If a keyword is provided, add a search condition
+      if (keyword) {
+        query = {
+          $or: [
+            { name: { $regex: new RegExp(keyword, 'i') } }, // Case-insensitive name search
+            { categori: { $regex: new RegExp(keyword, 'i') } }, // Case-insensitive category search
+            { 'ingredients.name': { $regex: new RegExp(keyword, 'i') } }, // Case-insensitive ingredient name search
+            // Add more fields to search if needed
+          ],
+        };
       }
+  
+      // Use the query to find products
+      const products = await Product.find(query);
+  
+      res.status(200).json({ data: products });
+    } catch (err) {
+      res.status(500).json({ message: err.message || 'Internal server error' });
+    }
   },
+  
 
    
 }
