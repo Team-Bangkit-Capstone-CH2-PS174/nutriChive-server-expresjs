a05fafa1badfaf4efb68c3ec61cdab02e94db9fc
DaniHidayat <danihidayat015@gmail.com>
2023-12-07 13:25:48 +0700

    add favorite

--- app/product/controller.js
+++ app/product/controller.js
@@ -1,20 +1,29 @@
-const Product = require('./model')
+const { Product,Favorite } = require('./model');
 const bcrypt = require('bcryptjs')
 
 module.exports = {
-    listProduk: async (req, res) => {
-        try {
-          const page = parseInt(req.query.page) || 1; 
-          const limit = parseInt(req.query.limit) || 99;
-          const skip = (page - 1) * limit;
-          const products = await Product.find().skip(skip).limit(limit);
-          res.status(200).json({data:products});
-         
+  listProduk: async (req, res) => {
+    try {
 
-        } catch (err) {
-            res.status(500).json({message: err.message || 'lah internal server error '})
-        }
-    },
+        const page = parseInt(req.query.page) || 1; 
+        const limit = parseInt(req.query.limit) || 99;
+        const skip = (page - 1) * limit;
+
+        // Retrieve products and total count from the database
+        const [products, totalCount] = await Promise.all([
+            Product.find().skip(skip).limit(limit).exec(),
+            Product.countDocuments().skip(skip).limit(limit), // Count total number of documents based on the same skip and limit
+        ]);
+
+        res.status(200).json({
+            data: products,
+            totalCount: totalCount,
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'Internal server error' });
+    }
+},
+
     listProdukDetail: async (req, res) => {
       try {
           const { id } = req.params;
@@ -54,6 +63,64 @@ module.exports = {
       res.status(500).json({ message: err.message || 'Internal server error' });
     }
   },
+  
+  favorite: async (req, res) => {
+    try {
+      const body = req.body;
+      const userId = req.user._id;
+      const productId = body.product;
+  
+      // Check if the favorite already exists for the user and product
+      const existingFavorite = await Favorite.findOne({ user: userId, product: productId });
+  
+      if (existingFavorite) {
+        return res.status(400).json({
+          message: 'Favorite already exists for this user and product.',
+        });
+      }
+  
+      // If the favorite does not exist, save it
+      const payload = {
+        user: userId,
+        product: productId,
+      };
+  
+      const favorite = new Favorite(payload);
+      await favorite.save();
+  
+      res.status(201).json({
+        data: favorite,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message || 'Internal server error' });
+    }
+  },
+  unfavorite: async (req, res) => {
+    try {
+      const body = req.body;
+      const userId = req.user._id;
+      const productId = body.product;
+  
+      // Check if the favorite exists for the user and product
+      const existingFavorite = await Favorite.findOne({ user: userId, product: productId });
+  
+      if (!existingFavorite) {
+        return res.status(404).json({
+          message: 'Favorite not found for this user and product.',
+        });
+      }
+  
+      // If the favorite exists, remove it
+      await existingFavorite.remove();
+  
+      res.status(200).json({
+        message: 'Unfavorited successfully.',
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message || 'Internal server error' });
+    }
+  }
+  
   
 
    
--- app/product/router.js
+++ app/product/router.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
-const { listProduk,listProdukDetail,listProdukSearch } = require('./controller');
+const { isLoginUser } = require('../middleware/auth');
+const { listProduk,listProdukDetail,listProdukSearch,favorite,unfavorite } = require('./controller');
 /* GET home page. */
-router.get('/', listProduk);
+router.get('/',listProduk);
 router.get('/detail/:id', listProdukDetail);
 router.get('/search', listProdukSearch);
+router.post('/favorite', isLoginUser, favorite);
+router.post('/unfavorite', isLoginUser, unfavorite);
 module.exports = router;
