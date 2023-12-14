a0426e48d6d493ab2a9f2e3b9c45daba90e5600c
DaniHidayat <danihidayat015@gmail.com>
2023-12-14 22:41:55 +0700

    push router

--- app/product/router.js
+++ app/product/router.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const { isLoginUser } = require('../middleware/auth');
-const { listProduk,listProdukDetail,listProdukSearch,favorite,unfavorite,listfavorite } = require('./controller');
+const { listProduk,listProdukDetail,listProdukSearch,favorite,unfavorite,listfavorite,listFavSearch } = require('./controller');
 /* GET home page. */
 router.get('/',listProduk);
 router.get('/detail/:id', listProdukDetail);
 router.get('/search', listProdukSearch);
 router.get('/listfavorite',isLoginUser, listfavorite);
+router.get('/searchFav',isLoginUser, listFavSearch);
 router.post('/favorite', isLoginUser, favorite);
 router.post('/unfavorite', isLoginUser, unfavorite);
 
